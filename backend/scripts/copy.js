5772e229546c7b874f7f17ca33ea6a2acc68e288
JDB <legodude17@users.noreply.github.com>
2018-10-05 10:00:15 -0700

    Fix buy, fix frontend build, add sell

--- backend/scripts/copy.js
+++ backend/scripts/copy.js
@@ -13,17 +13,25 @@ try {
   console.warn('Running Copy Process');
 }
 
-const deps = JSON.parse(fs.readFileSync(path.join(backend, 'package.json'), 'utf8')).dependencies;
+const pkg = JSON.parse(fs.readFileSync(path.join(backend, 'package.json'), 'utf8'));
 
 fs.writeFileSync(
   path.join(dist, 'package.json'),
   JSON.stringify({
-    dependencies: deps,
-    name: 'LOL, you have to compile the server',
-    version: '0.0.1'
+    dependencies: pkg.dependencies,
+    name: 'crasher-server',
+    version: pkg.version
   })
 );
 
 fs.writeFileSync(path.join(dist, 'config.json'), fs.readFileSync(path.join(backend, 'config.json'), 'utf8'));
 
+try {
+  fs.unlinkSync(path.join(dist, 'package-lock.json'));
+} catch (e) {
+  (_ => _)();
+}
+
 fs.writeFileSync(path.join(dist, `.${CHBD}`), CHBD);
+
+process.exit(0);
