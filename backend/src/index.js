504aaae066ae2860cbf5440586df7a344bef775c
JDB <legodude17@users.noreply.github.com>
2018-10-05 12:00:56 -0700

    Fixing some things

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,9 @@ import PouchDB from 'pouchdb';
 import { google } from 'googleapis';
 import fetch from 'node-fetch';
 import { IEXClient } from 'iex-api';
+import pfind from 'pouchdb-find';
+
+PouchDB.plugin(pfind);
 
 const app = express();
 const users = new PouchDB('users');
@@ -19,7 +22,7 @@ const publicURL = `${config.publicURL}:${config.port}`;
 const scope = [
   'profile',
   'https://www.googleapis.com/auth/userinfo.email',
-  'https://www.googleapis.com/auth/userinfo.profile'
+  'https://www.googleapis.com/auth/userinfo.profile',
 ];
 
 app.use(express.static(path.join(__dirname, 'public'), { index: false }));
@@ -28,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(session({
   secret: config.secret,
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -39,38 +42,38 @@ passport.deserializeUser((id, done) => users.get(id).then(user => done(null, use
 passport.use(new GoogleStrategy({
   clientID: config.clientID,
   clientSecret: config.clientSecret,
-  callbackURL: `${publicURL}/auth/google/callback`
+  callbackURL: `${publicURL}/auth/google/callback`,
 }, (accessToken, refreshToken, profile, cb) => {
   const client = new google.auth.OAuth2();
   client.setCredentials({
     access_token: accessToken,
-    refresh_token: refreshToken
+    refresh_token: refreshToken,
   });
   const people = google.people({
     version: 'v1',
-    auth: client
+    auth: client,
   });
   users.get(profile.id)
-    .then(user => {
+    .then((user) => {
       user.tokens.accessToken = accessToken;
       user.tokens.refreshToken = refreshToken;
       users.put(user).then(() => cb(null, user)).catch(cb);
     })
-    .catch(err => {
+    .catch((err) => {
       if (err.status === 404) {
         const user = {
           _id: profile.id,
           name: profile.displayName,
           tokens: {
-            accessToken, refreshToken
+            accessToken, refreshToken,
           },
           stocks: [],
           money: 0,
-          history: []
+          history: [],
         };
         people.people.get({
           resourceName: 'people/me',
-          personFields: 'emailAddresses'
+          personFields: 'emailAddresses',
         }).then(({ data }) => {
           user.email = data.emailAddresses[0].value;
           return users.put(user);
@@ -107,27 +110,27 @@ app.get('/api/stock/:ticker', (req, res) => {
   }
   const stock = {};
   iex.stockCompany(ticker)
-    .then(company => {
+    .then((company) => {
       stock.company = company;
       return iex.stockPrice(ticker);
     })
-    .then(price => {
+    .then((price) => {
       stock.price = price;
       return includeChart ? iex.stockChart(ticker, '1m') : false;
     })
-    .then(chart => {
+    .then((chart) => {
       if (chart) {
         stock.chart = chart;
       }
       return includeNews ? iex.stockNews(ticker, 2) : false;
     })
-    .then(news => {
+    .then((news) => {
       if (news) {
         stock.news = news;
       }
       res.send(stock);
     })
-    .catch(err => {
+    .catch((err) => {
       if (err.statusCode) {
         return res.status(err.statusCode).send(err.statusText || 'External server error');
       }
@@ -151,7 +154,7 @@ app.get('/api/buy/:ticker', (req, res) => {
   }
   let stock = stocks.length === 0 ? null : stocks[0];
   iex.stockPrice(ticker)
-    .then(price => {
+    .then((price) => {
       if (req.user.history == null) req.user.history = [];
       const onMargin = req.user.money < price * amount;
       req.user.history.push({
@@ -159,7 +162,7 @@ app.get('/api/buy/:ticker', (req, res) => {
         ticker,
         amount,
         price,
-        onMargin
+        onMargin,
       });
       if (stock) {
         if (onMargin) {
@@ -175,13 +178,13 @@ app.get('/api/buy/:ticker', (req, res) => {
         ticker,
         price,
         amount,
-        onMargin
+        onMargin,
       };
       req.user.stocks.push(stock);
       return users.put(req.user);
     })
     .then(done => done && res.send(stock))
-    .catch(err => {
+    .catch((err) => {
       if (err.statusCode) {
         return res.status(err.statusCode).send(err.statusText || 'External server error');
       }
@@ -218,13 +221,13 @@ app.get('/api/sell/:ticker', (req, res) => {
     res.status(400).send('Cannot sell more than owned');
   }
   iex.stockPrice(ticker)
-    .then(price => {
+    .then((price) => {
       req.user.history.push({
         type: 'sell',
         ticker,
         amount,
         price,
-        onMargin: stock.onMargin
+        onMargin: stock.onMargin,
       });
       stock.amount -= amount;
       req.user.money += price * amount;
@@ -237,12 +240,23 @@ app.get('/api/sell/:ticker', (req, res) => {
       return users.put(req.user);
     })
     .then(() => res.send('OK'))
-    .catch(err => {
+    .catch((err) => {
       if (err.statusCode) {
         return res.status(err.statusCode).send(err.statusText || 'External server error');
       }
       return res.status(500).send('Internal server error');
     });
+});
+
+app.get('/api/user/:email', (req, res) => {
+  const { email } = req.params;
+});
+
+app.get('/api/users', (req, res) => {
+  const limit = req.query.limit == null ? 1 : +req.query.limit;
+  if (!Number.isInteger(limit)) {
+    res.status(400).send('Amount must be an integer');
+  }
 });
 
 app.get('/', (req, res) => res.redirect('/login.html'));
