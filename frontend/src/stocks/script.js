8069ed44efd9cb9aced0d2b4d4ac31da4e7fbed8
JDB <legodude17@users.noreply.github.com>
2018-10-09 10:23:06 -0700

    Add search to stocks page

---
+++ frontend/src/stocks/script.js
@@ -0,0 +1,32 @@
+/* eslint-env browser */
+
+function list(id, arr, map) {
+  const ul = document.getElementById(id);
+  arr.map(map).map(t => {
+    const p = document.createElement('p');
+    p.innerText = t;
+    return p;
+  }).forEach(elm => ul.appendChild(elm));
+}
+
+window.onload = () => {
+  const search = document.getElementById('search');
+  const button = document.getElementById('doSearch');
+  button.onclick = () => {
+    const stocks = document.getElementById('stocks');
+    [...stocks.children].forEach(child => stocks.removeChild(child));
+    fetch(`/api/stocks/search/${search.value}`)
+      .then(res => res.json())
+      .then(res => {
+        const proms = res.stocks.map(stock => fetch(`/api/stock/${stock}`).then(res => res.json()));
+        Promise.all(proms).then(reses => {
+          list(
+            'stocks',
+            res.stocks,
+            (stock, i, _, stockData = reses[i]) => `${stockData.company.companyName} (${stock}) - $${stockData.price}`
+          );
+        });
+      })
+      .then(() => console.log('DONE!'));
+  };
+};
