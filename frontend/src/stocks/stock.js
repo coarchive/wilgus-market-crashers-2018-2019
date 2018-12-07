bff03a7925dd4850513bccbe530c4413e9fd6f95
JDB <legodude17@users.noreply.github.com>
2018-12-07 11:57:28 -0800

    Make new files

--- frontend/src/stocks/stock.js
+++ frontend/src/stocks/stock.js
@@ -1,5 +1,5 @@
 /* eslint-env browser */
-import Chart from 'chart.js';
+import Chart from "chart.js";
 
 // const write = (id) => {
 //   const e = document.getElementById(id);
@@ -10,111 +10,111 @@ function write(id, text) {
 }
 
 const s = window.location.search;
-const stock = s.slice(s.indexOf('stock=') + 6, s.includes('&') ? s.indexOf('&', s.indexOf('stock=')) : undefined);
+const stock = s.slice(s.indexOf("stock=") + 6, s.includes("&") ? s.indexOf("&", s.indexOf("stock=")) : undefined);
 
-Promise.all([fetch(`/api/stock/${stock}?chart=true`), fetch('/api/user')])
+Promise.all([fetch(`/api/stock/${stock}?chart=true`), fetch("/api/user")])
   .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
   .then(([stock, user]) => {
     document.title = `${stock.company.companyName} (${stock.company.symbol})`;
-    write('stock', `${document.title} - $${stock.price}`);
-    const ctx = document.getElementById('chart').getContext('2d');
+    write("stock", `${document.title} - $${stock.price}`);
+    const ctx = document.getElementById("chart").getContext("2d");
     const chart = new Chart(ctx, {
-      type: 'line',
+      type: "line",
       data: {
         labels: stock.chart.map(c => c.date),
         datasets: [{
           label: stock.company.symbol,
-          borderColor: '#0a0',
-          data: stock.chart.map(c => c.close)
-        }]
+          borderColor: "#0a0",
+          data: stock.chart.map(c => c.close),
+        }],
       },
       options: {
         responsive: false,
         title: {
           display: true,
-          text: 'Value over past Month'
+          text: "Value over past Month",
         },
         tooltips: {
-          mode: 'index',
-          intersect: false
+          mode: "index",
+          intersect: false,
         },
         hover: {
-          mode: 'nearest',
-          intersect: true
+          mode: "nearest",
+          intersect: true,
         },
         scales: {
           xAxes: [{
             display: true,
             scaleLabel: {
               display: true,
-              labelString: 'Day'
-            }
+              labelString: "Day",
+            },
           }],
           yAxes: [{
             display: true,
             scaleLabel: {
               display: true,
-              labelString: 'Value'
-            }
-          }]
-        }
-      }
+              labelString: "Value",
+            },
+          }],
+        },
+      },
     });
     chart.render({
       duration: 800,
       lazy: false,
-      easing: 'easeOutBounce'
+      easing: "easeOutBounce",
     });
-    document.getElementById('add').onclick = () => {
-      const newStock = prompt('What stock to add?'); // TODO: Something better
+    document.getElementById("add").onclick = () => {
+      const newStock = prompt("What stock to add?"); // TODO: Something better
       fetch(`/api/stock/${newStock}?chart=true`)
         .then(res => res.json())
         .then(newStock => {
           const newDataset = {
             label: newStock.company.symbol,
-            borderColor: '#a00',
-            data: newStock.chart.map(c => c.close)
+            borderColor: "#a00",
+            data: newStock.chart.map(c => c.close),
           };
           chart.config.data.datasets.push(newDataset);
           chart.update();
         });
     };
-    const feedback = document.getElementById('feedback');
-    const type = document.getElementById('type');
-    const howMuch = document.getElementById('much');
-    const go = document.getElementById('go');
+    const feedback = document.getElementById("feedback");
+    const type = document.getElementById("type");
+    const howMuch = document.getElementById("much");
+    const go = document.getElementById("go");
     const ticker = stock.company.symbol;
-    const ownedP = document.getElementById('owned');
+    const ownedP = document.getElementById("owned");
     function recalcFeedback() {
-      if (type.value === 'buy') {
+      if (type.value === "buy") {
         const cost = howMuch.value * stock.price;
-        feedback.innerText = `This will cost you $${cost}. You will ${cost < user.money ? 'not ' : ''}need a loan.`;
+        feedback.innerText = `This will cost you $${cost}. You will ${cost < user.money ? "not " : ""}need a loan.`;
         go.disabled = false;
       } else {
         const owned = user.stocks.filter(s => s.ticker === ticker)[0];
         if (owned == null) {
-          feedback.innerText = 'Can\'t sell something you don\'t own!';
+          feedback.innerText = "Can't sell something you don't own!";
           go.disabled = true;
         } else if (howMuch.value > owned.amount) {
-          feedback.innerText = 'Can\'t sell more than owned.';
+          feedback.innerText = "Can't sell more than owned.";
           go.disabled = true;
         } else {
           const earned = howMuch.value * stock.price;
           feedback.innerTest = `This will give you $${earned}.${owned.onMargin
             ? ` You will need to pay off the loan with $${howMuch.value * owned.price}.`
-            : ''}`;
+            : ""}`;
           go.disabled = true;
         }
       }
     }
-    if (user.error === 'Unauthorized') {
-      document.getElementById('transact').style.display = 'none';
-      ownedP.innerText = '';
+    if (user.error === "Unauthorized") {
+      document.getElementById("transact").style.display = "none";
+      ownedP.innerText = "";
     } else {
       const owned = user.stocks.filter(s => s.ticker === ticker)[0];
       type.onchange = howMuch.onchange = recalcFeedback;
       if (owned == null) {
-        ownedP.innerText = '';
+        ownedP.innerText = "";
       } else {
         ownedP.innerText = `You own ${owned.amount} shares in ${ticker}.`;
       }
